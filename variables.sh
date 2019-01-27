699edbb0446bf08802e29c70772ec408ec470405
ranjit-manutd <rnjitnetworking@gmail.com>
2019-01-27 01:10:59 -0500

    Adding variables.sh to understand about userdefind and system variables

---
+++ variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Using System Variables
+
+echo "Our Bash is : $BASH"
+echo "Our Bash Version is : $BASH_VERSION"
+echo "Our Home is : $HOME"
+echo "Our Present Working Directory is : $PWD"
+
+#Using User Defind Variables
+
+name=Pogba
+
+echo "Name is : $name"
